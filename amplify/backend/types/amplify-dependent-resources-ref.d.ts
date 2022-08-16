2d25c215006e830b0c2c971a86f6970c31363618
Pablo <pablo.ruiz.andino@protonmail.com>
2022-08-16 15:17:31 +0200

    add img

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyappreact": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "pic": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://nsuynw3gxfa2vm3yrefg3vwebm.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ohn6bytyyrgvlc5ubpwd7wnwxe",
+    "aws_user_files_s3_bucket": "amplifytestbuckernote134303-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
